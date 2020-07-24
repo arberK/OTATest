06505a651e9b5ec16f0ede9aadfe6b6b310b48e0
arber.kryeziu <arberkryeziu@live.com>
2020-07-24 12:03:04 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>OTAUpdates ..</Text>
+      <Image
+        source={require("./assets/logo.png")}
+        style={{ width: 100, height: 100 }}
+        resizeMode="contain"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
